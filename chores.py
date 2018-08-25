6d169b48842f440bcf37bc70226e72d757ffae65
baileympearson <bailey.pearson@vanderbilt.edu>
2018-08-24 21:44:01 -0500

    Created static file chores.py

---
+++ chores.py
@@ -0,0 +1,7 @@
+CHORES = {
+    'kitchen' : ['bailey','keaton','arturo','jordyn'],
+    'recycling' : ['eric','nathan'],
+    'trash' : ['joe'],
+    'sweeping/mopping' : ['ricky'],
+    'general pickup' : ['nate','lucy']
+}
