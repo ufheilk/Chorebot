e0956b9b621ccfca2bdbe307794547ef59269888
baileympearson <bailey.pearson@vanderbilt.edu>
2018-08-26 20:58:30 -0500

    Added chores.py

--- chores.py
+++ chores.py
@@ -1,4 +1,7 @@
 import datetime
+import random
+import json
+from chore_assignments import CHORES_TO_PEOPLE
 
 CHORE_OPTIONS = (
 'kitchen','sweeping/mopping','trash','general pickup','recycling'
@@ -12,14 +15,58 @@ CHORE_FREQUENCIES = {
 'recycling' : 2
 }
 
+def get_random_greeting():
+    random_greetings = ["Howdy","Buenas Dias","Sup fuckhead","Hey ballgobbler","Good'day",
+                        "Eat my ballsack"]
+    return random.choice(random_greetings)
+
+
 class Chore:
 
-    def __init__(self, chore,person):
-        self.chore = chore
-        self.chore_frequency = CHORE_FREQUENCIES[self.chore]
-        self.person = person
+    def update_person(self):
+        people = CHORES_TO_PEOPLE[self.person]
+        index = people.index(self.person)
 
-        self.next_active_day = datetime.datetime.today()
+        self.person = people[(index + 1) % len(people)]
+        
+    @classmethod
+    def from_dict(cls, dict):
+        ''' Initializes a chore from a dict '''
+
+        obj = cls()
+        for key in dict.keys():
+            obj.__dict__[key] = dict[key]
+
+        obj.__dict__['active'] = obj.next_active_day == datetime.datetime.today()
+        
+        return obj
+
+    def to_dict(self):
+        ''' 
+            Returns a custom dictionary representation of the 
+            chore, to use to convert the chore to json
+        '''
+        ret = { 'chore'             :   self.chore,
+                'chore_frequency'   :   self.chore_frequency, 
+                'next_active_day'   :   self.next_active_day,
+                'person'            :   self.person
+              }
+        return ret
 
     def active(self):
-        return self.next_active_day == datetime.datetime.today()
+        return self.active
+
+    def accountability_message(self):
+        return "{} is responsible for {} today.".format(self.person,self.chore)
+
+    def chore_message(self):
+        return "{} {}, please do your chore: {}".format(get_random_greeting(),self.person,self.chore)
+
+def initialize_chores(filename):
+    with open(filename) as f_obj:
+        chores = json.load(f_obj)
+        return chores
+
+for chore in initialize_chores('chores.json'):
+    tmp = Chore.from_dict(chore)
+    print(tmp.chore_message())
--- person.py
+++ person.py
@@ -6,9 +6,15 @@ class Person:
 		self.phone_email = phone_email
 		self.chore = chore
 		self.chore_index = chore_index
-		self.date_to_complete = datetime.datetime.today()
+
+		''' As of time of writing, I don't think we need this field '''
+		# self.date_to_complete = datetime.datetime.today()
 
 		self.reminder = True
 		self.completed = False
-		self.weekday_reminder = ''
-		self.weekend_reminder = ''
+		self.weekday_messages = ''
+		self.weekend_messages = ''
+	
+	def get_message_time(self):
+		today = datetime.datetime.today()
+		return self.weekday_messages if today.weekday() < 5 else self.weekend_messages
