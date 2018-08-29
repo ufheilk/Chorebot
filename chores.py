2177777240b9b96c432fd4df87879bfbfd9042d7
Bailey Pearson <bailey.pearson@vanderbilt.edu>
2018-08-29 16:17:50 -0500

    I think chores class is finished. Ready to test I believe

--- chores.py
+++ chores.py
@@ -10,14 +10,12 @@ CHORE_OPTIONS = (
 )
 
 CHORE_FREQUENCIES = {
-'kitchen' : datetime.timedelta(1),
-'sweeping/mopping' : datetime.timedelta(7),
-'trash' : 1,
-'general pickup' : 3,
-'recycling' : 2
+'kitchen' : datetime.timedelta(days=1),
+'sweeping/mopping' : datetime.timedelta(days=7),
+'trash' : datetime.timedelta(days=1),
+'general pickup' : datetime.timedelta(days=3),
+'recycling' : datetime.timedelta(days=2)
 }
-
-
 
 class Chore:
 
@@ -26,6 +24,14 @@ class Chore:
         index = people.index(self.person)
 
         self.person = people[(index + 1) % len(people)]
+	
+	def update_date(self):
+		if self.active:
+			self.next_active_day += CHORE_FREQUENCIES[self.chore]
+	
+	def update_chore(self):
+		self.update_person()
+		self.update_date()
         
     @classmethod
     def from_dict(cls, dict):
@@ -34,7 +40,7 @@ class Chore:
         obj = cls()
         for key in dict.keys():
             obj.__dict__[key] = dict[key]
-
+		obj.__dict__['next_active_day'] = datetime.datetime.strptime(obj['next_active_day'],"%m/%d/%Y")
         obj.__dict__['active'] = obj.next_active_day == datetime.datetime.today()
         
         return obj
