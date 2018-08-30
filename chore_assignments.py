0c977ecd67d45f0db5884022f438236bed6a141c
ufheilk <keaton.ufheil@vanderbilt.edu>
2018-08-29 22:34:48 -0500

    Final assignments

--- chore_assignments.py
+++ chore_assignments.py
@@ -13,8 +13,35 @@ PEOPLE_TO_CHORES = {
 	'jordyn' : 'kitchen',
 	'eric' : 'recycling',
 	'nathan' : 'recycling',
-	'trash' : 'joe',
+	'joe' : 'trash',
 	'ricky' : 'sweeping/mopping',
 	'nate' : 'general pickup',
 	'lucy' : 'general pickup'
+}
+
+VALID_RECEIVING_ADDRESSES = {
+    'bailey' : '5037087892@vzwpix.com',
+    'keaton' : '9543043368@mms.att.net',
+    'arturo' : 'no',
+    'jordyn' : '6162990280@vzwpix.com',
+    'eric' : '3523396234@mms.att.net',
+    'nathan' : '7062967760@mms.att.net',
+    'joe' : '6309674581@vzwpix.com',
+    'ricky' : '6309674581@vzwpix.com',
+    'nate' : '6365448948@mms.att.net',
+    'lucy' : '9787603833@vzwpix.com'
+}
+
+VALID_RECEIVING_ADDRESSES = {
+    '5037087892@vzwpix.com' : 'bailey',
+    '9543043368@mms.att.net' : 'keaton',
+    '6162990280@vzwpix.com' : 'jordyn',
+    '3523396234@mms.att.net' : 'eric',
+    '7062967760@mms.att.net' : 'nathan',
+    '7062967760@mms.att.net' : 'joe',
+    '6309674581@vzwpix.com' : 'ricky',
+    '6365448948@mms.att.net' : 'nate',
+    'nathan.o.glugla@vanderbilt.edu' : 'nate', # curse nate's phone
+    '9787603833@vzwpix.com' : 'lucy',
+    '5614454494@vzwpix.com' : 'arturo'
 }
