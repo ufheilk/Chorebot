232cdcd98dde431c3b92b14b587bf5d9d5cc2893
Eric Yeats <eric.c.yeats@vanderbilt.edu>
2018-09-17 20:35:56 -0500

    Made chorebot executable and added simple logging in chorebot.py

--- chorebot.py
+++ chorebot.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3
+
 import datetime
 import random
 import time
@@ -5,11 +7,28 @@ import threading
 
 import chores
 import person
-from collection import Messager, Mailbox
+from connection import Messager, Mailbox
 
 from helpers import anger_message,initialize_chores,initialize_people
 
 from credentials import credentials
+
+import logging
+
+logDir = ".dir"
+
+now = datetime.datetime.now()
+logFile = '.' + now.strftime("%a_%m_%d").lower() + '.log'
+
+logging.basicConfig(filename=logFile,level=logging.DEBUG)
+
+logging.debug('This message should go to the log file')
+logging.info('So should this')
+logging.warning('And this, too')
+
+
+exit()
+exit()
 
 # sleep until this time
 def sleep_until(time):
@@ -51,10 +70,16 @@ def check_mail(people):
             continue
         break
 
+logging.debug('Initializing Chorebot...')
+logging.debug('calling initialize_people')
+
 # First, initialize all Person and Chore objects from files
 
 # people: list of person objects
 people = initialize_people()
+
+logging.debug('returned from initialize_people')
+logging.debug('calling from initialize_people')
 
 # chores: list of chore objects
 chores = initialize_chores()
@@ -67,10 +92,14 @@ for chore in chores:
         # today's choredoer will be communicated to the others
         accountability_msg += chore.accountability_msg()
 
+logging.debug('dispatching threads to send the accountability messages')
+
 for person in people:
     if person.recv_accountability:
         # dispatch a thread to send this message at the desired time
         threading.Thread(target=send_timed_message, args=(person, accountability_msg).start())
+
+logging.debug('dispatching threads to send the chore messages')
 
 # send out the daily chore messages
 for chore in chores:
@@ -79,20 +108,25 @@ for chore in chores:
         msg = chore.chore_msg()
         threading.Thread(target=send_timed_message, args=(person, accountability_msg).start())
     
+logging.debug('chorebot enters its eternal slumber (until 10pm, that is)')
 # chorebot now enters its eternal slumber (until 10, that is)
 sleep_until('22:00')
 
+logging.debug('AWAKEN CHOREBOT')
+
 check_mail(people)
 
+logging.debug('send the slacking peons a reminder to do their chore')
 for person in people:
     if not person.chore_done:
         # sending this slacking peon a reminder
         send_message(person, "Please remember to do your chore")
         
-
+logging.debug("sleep until 'morn")
 # give these fools a little more time
 sleep_until('04:30')
 
+logging.debug('judgement day comences')
 check_mail(people)
 
 for person in people:
@@ -101,6 +135,10 @@ for person in people:
         send_message(person, anger_message(person.failure))
         person.failure += 1
 
+logging.debug('shame messages completed. saving state...')
+
 # serialize everything to a file
 serialize_people(people)
 serialize_chores(chores)
+
+logging.debug('chorelord slumbers until tomorrow')
